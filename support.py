33655cc7156d56ee95e861ed1a2eb655b77ed1b9
jeswanthyda <jy3012@columbia.edu>
2020-06-03 10:43:06 -0400

    structure update

--- support.py
+++ support.py
@@ -3,7 +3,8 @@ from utils import MDB,TradeStrategy
 if __name__ = "__main":
     while True:
         #Step1 - update hot stocks - should be one line call (check for 9 am and tackling inventory has to happen in some class method)
-        #Step2 - get all stock symbols - one liner
+        #Step2 - get all inventory and update current values of stocks in inventory and stock value in current data - one liner
+        #Step3 - get all stock symbols - one liner
         # Inside for loop
-            #Step3 - generate action signal for stock - one liner
-            #step4 - take corresponding action - one liner
+            #Step4 - generate action signal for stock - one liner
+            #step5 - take corresponding action - one liner
